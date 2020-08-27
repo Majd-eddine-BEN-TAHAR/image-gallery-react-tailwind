60b41e7564549385031c5d6ec6af9bf5d0b935ee
majd eddine ben tahar <majdeddinebentahar@gmail.com>
2020-08-27 10:08:04 +0100

    Updates

---
+++ precache-manifest.e7b38b171543a8dc991aca1942dd0762.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "337d0dc0e1053771197f5737e250bcfc",
+    "url": "/image-gallery-react-tailwind/index.html"
+  },
+  {
+    "revision": "63ac3441472a139fcaf6",
+    "url": "/image-gallery-react-tailwind/static/css/main.aa1d0ab6.chunk.css"
+  },
+  {
+    "revision": "a4677925e4d2c5a3a7e4",
+    "url": "/image-gallery-react-tailwind/static/js/2.c356ad44.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/image-gallery-react-tailwind/static/js/2.c356ad44.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63ac3441472a139fcaf6",
+    "url": "/image-gallery-react-tailwind/static/js/main.51356d0d.chunk.js"
+  },
+  {
+    "revision": "8ba61f2f1316760d95db",
+    "url": "/image-gallery-react-tailwind/static/js/runtime-main.5aa7c601.js"
+  },
+  {
+    "revision": "8051a858e1d5c19d12891fa4b97c5f5d",
+    "url": "/image-gallery-react-tailwind/static/media/search.8051a858.svg"
+  }
+]);
